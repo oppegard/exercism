75a9cf7b18884e707bd01f623950e99217983e4e
Glenn Oppegard <goppegard@pivotal.io>
2019-10-02 18:39:42 -0600

    [java] raindrops solution improvement

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -3,21 +3,10 @@ class RaindropConverter {
     String convert(int number) {
         String raindrops = "";
 
-        if (number % 3 == 0) {
-            raindrops += "Pling";
-        }
-
-        if (number % 5 == 0) {
-            raindrops += "Plang";
-        }
-
-        if (number % 7 == 0) {
-            raindrops += "Plong";
-        }
-
-        if (raindrops.isEmpty()) {
-            raindrops = Integer.toString(number);
-        }
+        if (number % 3 == 0) raindrops += "Pling";
+        if (number % 5 == 0) raindrops += "Plang";
+        if (number % 7 == 0) raindrops += "Plong";
+        if (raindrops.isEmpty()) raindrops = Integer.toString(number);
 
         return raindrops;
     }
