c1c84bf1cdf751ffbf9d615a457bad63f9951497
Glenn Oppegard <goppegard@pivotal.io>
2019-10-09 18:46:25 -0700

    [java] phone-number solution refactoring

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,32 +1,44 @@
-import java.util.regex.Pattern;
-
 public class PhoneNumber {
-    private String number = "";
+    private final String number;
 
     PhoneNumber(String input) {
-        this.number = "";
-
         checkUnpermittedCharacters(input);
+        input = onlyDigits(input);
+        input = checkLength(input);
 
-        for(Character character: input.toCharArray()) {
-            if(character.toString().matches("[0-9]")) this.number += character;
-        }
-
-        if (this.number.length() < 10) throw new IllegalArgumentException("incorrect number of digits");
-        if (this.number.length() == 11) {
-            if (!this.number.startsWith("1")) throw new IllegalArgumentException("11 digits must start with 1");
-            this.number = this.number.substring(1);
-        }
-        if (this.number.length() > 11) throw new IllegalArgumentException("more than 11 digits");
-        if (this.number.startsWith("0")) throw new IllegalArgumentException("area code cannot start with zero");
-        if (this.number.startsWith("1")) throw new IllegalArgumentException("area code cannot start with one");
-        if (this.number.startsWith("0", 3)) throw new IllegalArgumentException("exchange code cannot start with zero");
-        if (this.number.startsWith("1", 3)) throw new IllegalArgumentException("exchange code cannot start with one");
+        checkStartsWithZeroOrOne(input.substring(0, 3), "area code");
+        checkStartsWithZeroOrOne(input.substring(3, 6), "exchange code");
+        this.number = input;
     }
 
     private void checkUnpermittedCharacters(String input) {
         if (input.toLowerCase().matches(".*[a-z]+.*")) throw new IllegalArgumentException("letters not permitted");
         if (input.matches(".*[@,:,!]+.*")) throw new IllegalArgumentException("punctuations not permitted");
+    }
+
+    private String onlyDigits(String input) {
+        StringBuilder justDigits = new StringBuilder();
+        for (Character character : input.toCharArray())
+            if (character.toString().matches("[0-9]")) justDigits.append(character);
+        return justDigits.toString();
+    }
+
+    private String checkLength(String input) {
+        if (input.length() < 10) throw new IllegalArgumentException("incorrect number of digits");
+        if (input.length() > 11) throw new IllegalArgumentException("more than 11 digits");
+        if (input.length() == 11) {
+            if (input.startsWith("1")) {
+                input = input.substring(1);
+            } else {
+                throw new IllegalArgumentException("11 digits must start with 1");
+            }
+        }
+        return input;
+    }
+
+    private void checkStartsWithZeroOrOne(String digits, String thing) {
+        if (digits.startsWith("0")) throw new IllegalArgumentException(thing + " cannot start with zero");
+        if (digits.startsWith("1")) throw new IllegalArgumentException(thing + " cannot start with one");
     }
 
     public String getNumber() {
