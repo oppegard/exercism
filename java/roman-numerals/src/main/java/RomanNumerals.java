4edd66e7c1dc3789b740a67545b7478fa32eda1c
Glenn Oppegard <goppegard@pivotal.io>
2019-10-12 14:53:14 -0600

    [java] roman-numerals use static mappings

--- java/roman-numerals/src/main/java/RomanNumerals.java
+++ java/roman-numerals/src/main/java/RomanNumerals.java
@@ -2,35 +2,32 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 class RomanNumeral {
+    private final static LinkedHashMap<Integer, String> orderedNumeralMappings = new LinkedHashMap<>() {
+        {
+            put(1000, "M");
+            put(900, "CM");
+            put(500, "D");
+            put(400, "CD");
+            put(100, "C");
+            put(90, "XC");
+            put(50, "L");
+            put(40, "XL");
+            put(10, "X");
+            put(9, "IX");
+            put(5, "V");
+            put(4, "IV");
+            put(1, "I");
+        }
+    };
     private String romanNumeral = "";
 
     RomanNumeral(int value) {
-        final Map<Integer, String> orderedNumeralMappings = generateNumeralMappings();
-
         for (Map.Entry<Integer, String> entry : orderedNumeralMappings.entrySet()) {
             while (value - entry.getKey() >= 0) {
                 value = value - entry.getKey();
                 this.romanNumeral += entry.getValue();
             }
         }
-    }
-
-    private Map<Integer, String> generateNumeralMappings() {
-        Map<Integer, String> mappings = new LinkedHashMap<>();
-        mappings.put(1000, "M");
-        mappings.put(900, "CM");
-        mappings.put(500, "D");
-        mappings.put(400, "CD");
-        mappings.put(100, "C");
-        mappings.put(90, "XC");
-        mappings.put(50, "L");
-        mappings.put(40, "XL");
-        mappings.put(10, "X");
-        mappings.put(9, "IX");
-        mappings.put(5, "V");
-        mappings.put(4, "IV");
-        mappings.put(1, "I");
-        return mappings;
     }
 
     public String getRomanNumeral() {
