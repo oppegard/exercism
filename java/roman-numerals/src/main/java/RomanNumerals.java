8a433bb2975ed897c443967e3d60ff2389822f01
Glenn Oppegard <goppegard@pivotal.io>
2019-10-12 16:14:12 -0600

    [java] roman-numerals prettify

--- java/roman-numerals/src/main/java/RomanNumerals.java
+++ java/roman-numerals/src/main/java/RomanNumerals.java
@@ -4,19 +4,19 @@ import java.util.Map;
 class RomanNumeral {
     private final static LinkedHashMap<Integer, String> orderedNumeralMappings = new LinkedHashMap<>() {
         {
-            put(1000, "M");
-            put(900, "CM");
-            put(500, "D");
-            put(400, "CD");
-            put(100, "C");
-            put(90, "XC");
-            put(50, "L");
-            put(40, "XL");
-            put(10, "X");
-            put(9, "IX");
-            put(5, "V");
-            put(4, "IV");
-            put(1, "I");
+            put(1000, "M" );
+            put( 900, "CM");
+            put( 500, "D" );
+            put( 400, "CD");
+            put( 100, "C" );
+            put(  90, "XC");
+            put(  50, "L" );
+            put(  40, "XL");
+            put(  10, "X" );
+            put(   9, "IX");
+            put(   5, "V" );
+            put(   4, "IV");
+            put(   1, "I" );
         }
     };
     private String romanNumeral = "";
