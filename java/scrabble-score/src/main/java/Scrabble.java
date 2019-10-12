3887913b970e5cc79e1ca977b928e99bc137b67e
Glenn Oppegard <goppegard@pivotal.io>
2019-10-12 17:08:20 -0600

    [java] scrabble: more succint Map initialization

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,33 +1,22 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 class Scrabble {
+    private static final Map<Integer, Character[]> LETTER_VALUES = new HashMap<>() {
+        {
+            put( 1, new Character[]{'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'});
+            put( 2, new Character[]{'D', 'G'                                        });
+            put( 3, new Character[]{'B', 'C', 'M', 'P'                              });
+            put( 4, new Character[]{'F', 'H', 'V', 'W', 'Y'                         });
+            put( 5, new Character[]{'K',                                            });
+            put( 8, new Character[]{'J', 'X'                                        });
+            put(10, new Character[]{'Q', 'Z'                                        });
 
-    private static final Character[] ONE_POINTERS   = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'};
-    private static final Character[] TWO_POINTERS   = {'D', 'G'};
-    private static final Character[] THREE_POINTERS = {'B', 'C', 'M', 'P'};
-    private static final Character[] FOUR_POINTERS  = {'F', 'H', 'V', 'W', 'Y'};
-    private static final Character[] FIVE_POINTERS  = {'K'};
-    private static final Character[] EIGHT_POINTERS = {'J', 'X'};
-    private static final Character[] TEN_POINTERS   = {'Q', 'Z'};
-    private static final Map<Character[], Integer> LETTER_VALUES;
+            for (Character[] letterSet : values()) Arrays.sort(letterSet);
+        }
+    };
     private final String word;
-
-    static {
-        Map<Character[], Integer> aMap = new HashMap<>();
-        aMap.put(ONE_POINTERS,   1);
-        aMap.put(TWO_POINTERS,   2);
-        aMap.put(THREE_POINTERS, 3);
-        aMap.put(FOUR_POINTERS,  4);
-        aMap.put(FIVE_POINTERS,  5);
-        aMap.put(EIGHT_POINTERS, 8);
-        aMap.put(TEN_POINTERS,  10);
-        for (Character[] letterSet : aMap.keySet()) Arrays.sort(letterSet);
-
-        LETTER_VALUES = Collections.unmodifiableMap(aMap);
-    }
 
     Scrabble(String word) {
         this.word = word;
@@ -36,16 +25,13 @@ class Scrabble {
     int getScore() {
         int score = 0;
 
-        for (char letter : word.toCharArray()) {
-            letter = Character.toUpperCase(letter);
-
-            for (Map.Entry<Character[], Integer> entry : LETTER_VALUES.entrySet()) {
-                Character[] letterSet = entry.getKey();
-                Integer pointValue = entry.getValue();
+        for (char letter : word.toUpperCase().toCharArray()) {
+            for (Map.Entry<Integer, Character[]> entry : LETTER_VALUES.entrySet()) {
+                Integer pointValue = entry.getKey();
+                Character[] letterSet = entry.getValue();
                 if (Arrays.binarySearch(letterSet, letter) >= 0) score += pointValue;
             }
         }
         return score;
     }
-
 }
