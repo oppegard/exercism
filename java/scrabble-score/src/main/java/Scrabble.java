b3ba01cc86690186b6fb40bc650eb6c159fef5e8
Glenn Oppegard <goppegard@pivotal.io>
2019-10-12 17:36:25 -0600

    [java] scrabble: a Set makes more sense than Array

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,19 +1,15 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Scrabble {
-    private static final Map<Integer, Character[]> LETTER_VALUES = new HashMap<>() {
+    private static final Map<Integer, Set<Character>> LETTER_VALUES = new HashMap<>() {
         {
-            put( 1, new Character[]{'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'});
-            put( 2, new Character[]{'D', 'G'                                        });
-            put( 3, new Character[]{'B', 'C', 'M', 'P'                              });
-            put( 4, new Character[]{'F', 'H', 'V', 'W', 'Y'                         });
-            put( 5, new Character[]{'K',                                            });
-            put( 8, new Character[]{'J', 'X'                                        });
-            put(10, new Character[]{'Q', 'Z'                                        });
-
-            for (Character[] letterSet : values()) Arrays.sort(letterSet);
+            put( 1, new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')));
+            put( 2, new HashSet<>(Arrays.asList('D', 'G'                                        )));
+            put( 3, new HashSet<>(Arrays.asList('B', 'C', 'M', 'P'                              )));
+            put( 4, new HashSet<>(Arrays.asList('F', 'H', 'V', 'W', 'Y'                         )));
+            put( 5, new HashSet<>(Arrays.asList('K'                                             )));
+            put( 8, new HashSet<>(Arrays.asList('J', 'X'                                        )));
+            put(10, new HashSet<>(Arrays.asList('Q', 'Z'                                        )));
         }
     };
     private final String word;
@@ -23,13 +19,10 @@ class Scrabble {
     }
 
     int getScore() {
-        int score = 0;
-
-        for (char letter : word.toUpperCase().toCharArray()) {
-            for (Map.Entry<Integer, Character[]> entry : LETTER_VALUES.entrySet()) {
-                Integer pointValue = entry.getKey();
-                Character[] letterSet = entry.getValue();
-                if (Arrays.binarySearch(letterSet, letter) >= 0) score += pointValue;
+        Integer score = 0;
+        for (Character letter : word.toUpperCase().toCharArray()) {
+            for (Map.Entry<Integer, Set<Character>> entry : LETTER_VALUES.entrySet()) {
+                if (entry.getValue().contains(letter)) score += entry.getKey();
             }
         }
         return score;
