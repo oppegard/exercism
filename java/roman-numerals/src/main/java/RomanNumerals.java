29aa1ca2c5be665463f209cd02e9aaf96365866f
Glenn Oppegard <goppegard@pivotal.io>
2019-10-12 14:18:36 -0600

    [java] roman-numeral solution

--- java/roman-numerals/src/main/java/RomanNumerals.java
+++ java/roman-numerals/src/main/java/RomanNumerals.java
@@ -1,10 +1,39 @@
-/*
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class RomanNumeral {
+    private String romanNumeral = "";
 
-Please remove this comment when submitting your solution.
+    RomanNumeral(int value) {
+        final Map<Integer, String> orderedNumeralMappings = generateNumeralMappings();
 
-*/
+        for (Map.Entry<Integer, String> entry : orderedNumeralMappings.entrySet()) {
+            while (value - entry.getKey() >= 0) {
+                value = value - entry.getKey();
+                this.romanNumeral += entry.getValue();
+            }
+        }
+    }
+
+    private Map<Integer, String> generateNumeralMappings() {
+        Map<Integer, String> mappings = new LinkedHashMap<>();
+        mappings.put(1000, "M");
+        mappings.put(900, "CM");
+        mappings.put(500, "D");
+        mappings.put(400, "CD");
+        mappings.put(100, "C");
+        mappings.put(90, "XC");
+        mappings.put(50, "L");
+        mappings.put(40, "XL");
+        mappings.put(10, "X");
+        mappings.put(9, "IX");
+        mappings.put(5, "V");
+        mappings.put(4, "IV");
+        mappings.put(1, "I");
+        return mappings;
+    }
+
+    public String getRomanNumeral() {
+        return romanNumeral;
+    }
+}
