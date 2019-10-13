1f5446814e3b4bcff5d0bc72f983e394f975d72b
Glenn Oppegard <goppegard@pivotal.io>
2019-10-13 16:30:30 -0600

    [java] reverse-string: Pre-JDK5 solution

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -2,7 +2,7 @@ class ReverseString {
 
     String reverse(String inputString) {
         String reversed = "";
-        for (Character c : inputString.toCharArray()) reversed = c + reversed;
+        for (int i = inputString.length()-1; i >= 0; i--) reversed += inputString.charAt(i);
         return reversed;
     }
 
