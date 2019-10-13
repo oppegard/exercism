3b20ba82851b17edadb42294619ebb3ac94fe4cd
Glenn Oppegard <goppegard@pivotal.io>
2019-10-13 15:28:19 -0600

    [java] isbn-verifier: solution

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,25 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        stringToVerify = sanitizeInput(stringToVerify);
+        if (!validInput(stringToVerify)) return false;
+
+        int dividend = 0;
+        int counter = 0;
+        for (int multiplier = 10; multiplier > 0; multiplier--) {
+            int charToDigit = Character.getNumericValue(stringToVerify.charAt(counter++));
+            if (charToDigit == Character.getNumericValue('X')) charToDigit = 10;
+            dividend += charToDigit * multiplier;
+        }
+
+        return (dividend % 11 == 0);
     }
 
+    private String sanitizeInput(String input) {
+        return input.replace("-", "");
+    }
+
+    private boolean validInput(String input) {
+        return input.matches("[0-9]{9}[0-9,X]");
+    }
 }
