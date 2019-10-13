886089d174bc1d8b5b5c6b2e7dba6a5e5921d84e
Glenn Oppegard <goppegard@pivotal.io>
2019-10-13 16:03:24 -0600

    [java] isbn-verifier: refactor

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -2,23 +2,26 @@ class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
         stringToVerify = sanitizeInput(stringToVerify);
-        if (!validInput(stringToVerify)) return false;
+        if (!isValidFormat(stringToVerify)) return false;
 
-        int dividend = 0;
-        int multiplier = 10;
-        for (Character c : stringToVerify.toCharArray()) {
-            int charToNumeric = c == 'X' ? 10 : Character.getNumericValue(c);
-            dividend += charToNumeric * multiplier--;
-        }
-
-        return (dividend % 11 == 0);
+        int isbnSum = calculateIsbnSum(stringToVerify);
+        return (isbnSum % 11 == 0);
     }
 
     private String sanitizeInput(String input) {
         return input.replace("-", "");
     }
 
-    private boolean validInput(String input) {
-        return input.matches("[0-9]{9}[0-9,X]");
+    private boolean isValidFormat(String input) {
+        return input.matches("^[0-9]{9}[0-9,X]$");
+    }
+
+    private int calculateIsbnSum(String stringToVerify) {
+        int isbnSum = 0, multiplier = 10;
+        for (Character c : stringToVerify.toCharArray()) {
+            int numericValue = c == 'X' ? 10 : Character.getNumericValue(c);
+            isbnSum += numericValue * multiplier--;
+        }
+        return isbnSum;
     }
 }
