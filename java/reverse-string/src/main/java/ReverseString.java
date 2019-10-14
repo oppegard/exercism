94294f24b257cea5418ff36c721be1a4a84e1e9a
Glenn Oppegard <goppegard@pivotal.io>
2019-10-13 23:43:03 -0600

    [java] reverse-string: Stream API solution

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,9 +1,8 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        String reversed = "";
-        for (int i = inputString.length()-1; i >= 0; i--) reversed += inputString.charAt(i);
-        return reversed;
+        return inputString.chars()
+            .mapToObj(a -> Character.toString((char) a))
+            .reduce("", (a, b) -> b + a);
     }
-
 }
