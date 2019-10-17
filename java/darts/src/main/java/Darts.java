e61cb847b1df40c4e5357cffd24a47e57c43dba9
Glenn Oppegard <goppegard@pivotal.io>
2019-10-17 10:24:25 -0600

    [java] darts: solution

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,16 @@
 class Darts {
 
+    private double radius;
+
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        radius = Math.hypot(x, y);
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (radius <=  1) return 10;
+        if (radius <=  5) return  5;
+        if (radius <= 10) return  1;
+        return 0;
     }
 
 }
