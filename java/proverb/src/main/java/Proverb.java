6b60fda6696072f49196082298f7d712b829e038
Glenn Oppegard <goppegard@pivotal.io>
2019-10-17 10:00:04 -0600

    [java] proverb: for-loop solution

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,24 @@
 class Proverb {
 
+    private final String[] words;
+
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (words.length == 0) return "";
+
+        StringBuilder proverb = new StringBuilder();
+        for (int i = 0; i < words.length - 1; i++) {
+            proverb
+                .append("For want of a ")
+                .append(words[i])
+                .append(" the ")
+                .append(words[i + 1])
+                .append(" was lost.\n");
+        }
+        return proverb + "And all for the want of a " + words[0] + ".";
     }
 
 }
