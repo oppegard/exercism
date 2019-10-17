254db88ab86b12648d332cec25985fed0aa5825a
Glenn Oppegard <goppegard@pivotal.io>
2019-10-17 10:40:13 -0600

    [java] leap: solution

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,9 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (year %   4 != 0) return false;
+        if (year % 100 == 0) return year % 400 == 0;
+        return true;
     }
 
 }
