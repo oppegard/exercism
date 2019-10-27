719ec0d85b901bde5711e73adbb58de604cf7efe
Glenn Oppegard <goppegard@pivotal.io>
2019-10-27 17:44:35 -0600

    [java] difference-of-squares: magic solution
    https://www.geeksforgeeks.org/difference-between-sum-of-the-squares-of-first-n-natural-numbers-and-square-of-sum/

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,17 +1,16 @@
-import java.util.stream.IntStream;
-
 class DifferenceOfSquaresCalculator {
+    // Algorithm credit: https://www.geeksforgeeks.org/difference-between-sum-of-the-squares-of-first-n-natural-numbers-and-square-of-sum/
 
     int computeDifferenceOfSquares(int input) {
         return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
 
     int computeSquareOfSumTo(int input) {
-        int sum = IntStream.rangeClosed(1, input).sum();
+        int sum = (input * (input + 1)) / 2;
         return sum * sum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        return IntStream.rangeClosed(1, input).map(i -> i * i).sum();
+        return (input * (input + 1) * (2 * input + 1)) / 6;
     }
 }
