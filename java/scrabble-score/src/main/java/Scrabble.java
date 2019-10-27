2f059dee6bf665980ee2074af94537e396a7d318
Glenn Oppegard <goppegard@pivotal.io>
2019-10-27 17:04:07 -0600

    [java] scrabble: simplify to a single for-loop

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,15 +1,35 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Scrabble {
-    private static final Map<Integer, Set<Character>> LETTER_VALUES = new HashMap<>() {
+    private static final Map<Character, Integer> LETTER_VALUES = new HashMap<>() {
         {
-            put( 1, new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')));
-            put( 2, new HashSet<>(Arrays.asList('D', 'G'                                        )));
-            put( 3, new HashSet<>(Arrays.asList('B', 'C', 'M', 'P'                              )));
-            put( 4, new HashSet<>(Arrays.asList('F', 'H', 'V', 'W', 'Y'                         )));
-            put( 5, new HashSet<>(Arrays.asList('K'                                             )));
-            put( 8, new HashSet<>(Arrays.asList('J', 'X'                                        )));
-            put(10, new HashSet<>(Arrays.asList('Q', 'Z'                                        )));
+            put('A', 1);
+            put('B', 3);
+            put('C', 3);
+            put('D', 2);
+            put('E', 1);
+            put('F', 4);
+            put('G', 2);
+            put('H', 4);
+            put('I', 1);
+            put('J', 8);
+            put('K', 5);
+            put('L', 1);
+            put('M', 3);
+            put('N', 1);
+            put('O', 1);
+            put('P', 3);
+            put('Q', 10);
+            put('R', 1);
+            put('S', 1);
+            put('T', 1);
+            put('U', 1);
+            put('V', 4);
+            put('W', 4);
+            put('X', 8);
+            put('Y', 4);
+            put('Z', 10);
         }
     };
     private final String word;
@@ -21,9 +41,7 @@ class Scrabble {
     int getScore() {
         Integer score = 0;
         for (Character letter : word.toUpperCase().toCharArray()) {
-            for (Map.Entry<Integer, Set<Character>> entry : LETTER_VALUES.entrySet()) {
-                if (entry.getValue().contains(letter)) score += entry.getKey();
-            }
+            score += LETTER_VALUES.getOrDefault(letter, 0);
         }
         return score;
     }
