1159e567ba1ae8708998085fa17fb28a659923bd
Glenn Oppegard <goppegard@pivotal.io>
2019-11-01 12:52:20 -0600

    [java] yacht: wip

--- java/yacht/src/main/java/Yacht.java
+++ java/yacht/src/main/java/Yacht.java
@@ -1,11 +1,36 @@
+import java.util.stream.IntStream;
+
 class Yacht {
 
+    private final int[] dice;
+    private final YachtCategory yachtCategory;
+
     Yacht(int[] dice, YachtCategory yachtCategory) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.dice          = dice;
+        this.yachtCategory = yachtCategory;
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        switch (yachtCategory) {
+            case ONES:
+                return calculateN(1, dice);
+            case TWOS:
+                return calculateN(2, dice);
+            case THREES:
+                return calculateN(3, dice);
+            case FOURS:
+                return calculateN(4, dice);
+            case FIVES:
+                return calculateN(5, dice);
+            case SIXES:
+                return calculateN(6, dice);
+            default:
+                return 0;
+        }
+    }
+
+    private int calculateN(int n, int[] dice) {
+        return IntStream.of(dice).filter(i -> i == n).sum();
     }
 
 }
