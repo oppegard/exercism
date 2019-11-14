529fe1dd6966ff82880106b61c7657ea3094268f
Glenn Oppegard <goppegard@pivotal.io>
2019-11-14 15:06:41 -0700

    [go] hamming: solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,23 @@
+// Package hamming calculates Hamming distance
 package hamming
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+// Distance calculates the Hamming distance of two strings
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("length mismatch")
+	}
+
+	distance := 0
+	for i, aRune := range a {
+		bRune, _ := utf8.DecodeRuneInString(b[i:])
+		if aRune != bRune {
+			distance++
+		}
+	}
+	return distance, nil
 }
