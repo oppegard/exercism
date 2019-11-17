32193353c953e9e668ff8647063cf1896acad652
Glenn Oppegard <goppegard@pivotal.io>
2019-11-17 13:55:11 -0700

    [go] rna-transcription: rune->rune in map

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -4,17 +4,15 @@ package strand
 // ToRNA transcribes a DNA sequence to its RNA complement.
 func ToRNA(dna string) string {
 	var rna string
-
 	for _, nucleotide := range dna {
-		rna += dnaToRna[nucleotide]
+		rna += string(dnaToRna[nucleotide])
 	}
-
 	return rna
 }
 
-var dnaToRna = map[rune]string{
-	'G': "C",
-	'C': "G",
-	'T': "A",
-	'A': "U",
+var dnaToRna = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
 }
