43015faa3bbd44d4151e93887aefdc6dcf4ec494
Glenn Oppegard <goppegard@pivotal.io>
2019-11-17 13:45:00 -0700

    [go] rna-transcription: solution

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,20 @@
+// Package strand processes DNA strands.
 package strand
 
+// ToRNA transcribes a DNA sequence to its RNA complement.
 func ToRNA(dna string) string {
+	var rna string
+
+	for _, nucleotide := range dna {
+		rna += dnaToRna[nucleotide]
+	}
+
+	return rna
+}
+
+var dnaToRna = map[rune]string{
+	'G': "C",
+	'C': "G",
+	'T': "A",
+	'A': "U",
 }
