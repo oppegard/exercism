8702a8007fb7ac3157ae439027b2f290c748f710
Glenn Oppegard <goppegard@pivotal.io>
2019-11-20 13:30:38 -0800

    [go] gigasecond: clarity

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,12 +1,11 @@
 // Package gigasecond provides a worldview of gigaseconds.
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
+
+const gigasecond time.Duration = 1e9 * time.Second
 
 // AddGigasecond takes a Time and returns a Time with a gigasecond added to it.
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(gigasecond)
 }
-
-const gigasecond time.Duration = 1000000000000000000
