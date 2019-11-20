94cff4e998b26ed1cbe3899ae0f47cbe1fb67f15
Glenn Oppegard <goppegard@pivotal.io>
2019-11-20 11:34:54 -0800

    [go] raindrops: solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,1 +1,24 @@
+// Package raindrops converts numbers to the sounds of raindrops.
 package raindrops
+
+import "strconv"
+
+// Convert takes a number and converts into a string that contains raindrop
+// sounds corresponding to certain potential factors.
+func Convert(input int) string {
+	var raindrops string
+
+	if input%3 == 0 {
+		raindrops += "Pling"
+	}
+	if input%5 == 0 {
+		raindrops += "Plang"
+	}
+	if input%7 == 0 {
+		raindrops += "Plong"
+	}
+	if raindrops == "" {
+		raindrops = strconv.Itoa(input)
+	}
+	return raindrops
+}
