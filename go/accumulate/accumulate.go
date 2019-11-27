817f55b9d8209287b5ca0a31d776c2e8b4b1029f
Glenn Oppegard <goppegard@pivotal.io>
2019-11-26 20:40:22 -0700

    [go] accumulate: solution

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,1 +1,13 @@
 package accumulate
+
+// Converter is a type given to `Accumulate`, which is used to accumulate the result.
+type Converter func(string) string
+
+// Accumulate applies `converter` to every element of `words`, returning the result.
+func Accumulate(words []string, converter Converter) []string {
+	result := make([]string, len(words))
+	for i, word := range words {
+		result[i] = converter(word)
+	}
+	return result
+}
