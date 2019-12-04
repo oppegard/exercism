17d880cc3f665218f84a65c55e008c0e9d44f951
Glenn Oppegard <goppegard@pivotal.io>
2019-12-03 22:42:54 -0700

    [go] scrabble-score: implement mentor feedback

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,34 +1,22 @@
 // Package scrabble provides utilities for the game of Scrabble.
 package scrabble
 
-import (
-	"strings"
-	"unicode"
-)
+import "strings"
 
-var lettersToPoints = map[string]int{
-	"AEIOULNRST": 1,
-	"DG":         2,
-	"BCMP":       3,
-	"FHVWY":      4,
-	"K":          5,
-	"JX":         8,
-	"QZ":         10,
+var lettersToPoints = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
 
 // Score takes a word and returns its Scrabble score.
 func Score(word string) (score int) {
-	for _, letter := range word {
-		score += pointValueFor(letter)
+	for _, letter := range strings.ToUpper(word) {
+		score += lettersToPoints[letter]
 	}
 	return
-}
-
-func pointValueFor(r rune) int {
-	for letters, pointValue := range lettersToPoints {
-		if strings.ContainsRune(letters, unicode.ToUpper(r)) {
-			return pointValue
-		}
-	}
-	return 0
 }
