4f2a7baac2dd0f96e15c43e93cff5f8590033696
Glenn Oppegard <goppegard@pivotal.io>
2019-12-05 18:52:42 -0700

    [go] isogram: solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,21 @@
+// Package isogram determines if a word or phrase is an isogram.
+// More info: https://en.wikipedia.org/wiki/Isogram.
 package isogram
+
+import "unicode"
+
+// IsIsogram takes a word or phrase and determines if it's an isogram.
+func IsIsogram(input string) bool {
+	var state = map[rune]bool{}
+	for _, r := range input {
+		if r == '-' || r == ' ' {
+			continue
+		}
+		lowercase := unicode.ToLower(r)
+		if _, present := state[lowercase]; present {
+			return false
+		}
+		state[lowercase] = true
+	}
+	return true
+}
