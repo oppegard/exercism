076b496fc154b4fb7b70a6cb11ab922c83916ec4
Glenn Oppegard <goppegard@pivotal.io>
2019-12-06 22:18:42 -0700

    [go] roman-numerals: simplify more

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -5,51 +5,38 @@ import (
 	"strings"
 )
 
-var arabicToRoman = map[int]string{
-	1000: "M",
-	900:  "CM",
-	500:  "D",
-	400:  "CD",
-	100:  "C",
-	90:   "XC",
-	50:   "L",
-	40:   "XL",
-	10:   "X",
-	9:    "IX",
-	5:    "V",
-	4:    "IV",
-	1:    "I",
+type arabicToRoman struct {
+	arabic int
+	roman  string
 }
 
-// Can't `range` through the keys of the arabicToRoman map in the order they were
-// defined, so use this slice to define the order of values to iterate over.
-var descendingArabic = []int{
-	1000,
-	900,
-	500,
-	400,
-	100,
-	90,
-	50,
-	40,
-	10,
-	9,
-	5,
-	4,
-	1,
+var mappings = []arabicToRoman{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
 }
 
-func ToRomanNumeral(arabic int) (roman string, err error) {
-	if arabic <= 0 || arabic > 3000 {
+func ToRomanNumeral(input int) (result string, err error) {
+	if input <= 0 || input > 3000 {
 		return "", errors.New("invalid input")
 	}
 
-	for _, i := range descendingArabic {
-		if quotient := arabic / i; quotient == 0 {
+	for _, mapping := range mappings {
+		if quotient := input / mapping.arabic; quotient == 0 {
 			continue
 		} else {
-			roman += strings.Repeat(arabicToRoman[i], quotient)
-			arabic = arabic % i
+			result += strings.Repeat(mapping.roman, quotient)
+			input = input % mapping.arabic
 		}
 	}
 	return
