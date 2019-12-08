5536a2f35d5e353f25bd8a9e665f949bd12ecbbe
Glenn Oppegard <goppegard@pivotal.io>
2019-12-07 17:11:17 -0700

    [go] etl: solution

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,1 +1,16 @@
 package etl
+
+import (
+	"strings"
+)
+
+func Transform(m map[int][]string) map[string]int {
+	var result = make(map[string]int)
+	for point, letters := range m {
+		for _, letter := range letters {
+			lc := strings.ToLower(letter)
+			result[lc] = point
+		}
+	}
+	return result
+}
