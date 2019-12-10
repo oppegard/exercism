a1091b247a4a647b2467a72497529ce755fd0849
Glenn Oppegard <goppegard@pivotal.io>
2019-12-10 08:31:55 -0700

    [go] difference-of-squares: improve documentation

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,20 +1,22 @@
-// Package diffsquares computes the sum of squares.
+// Package diffsquares provides functions to compute squares of sums, sums of
+// squares, and difference between the square of the sum and the sum of the
+// squares of the first N natural numbers.
 // Algorithm credit: https://www.geeksforgeeks.org/difference-between-sum-of-the-squares-of-first-n-natural-numbers-and-square-of-sum/
 package diffsquares
 
-// SquareOfSum computes the square of the sum for the input.
+// SquareOfSum computes the square of the sum for the first N natural numbers.
 func SquareOfSum(n int) int {
 	sum := (n * (n + 1)) / 2
 	return sum * sum
 }
 
-// SumOfSquares computes the sum of squares for the input.
+// SumOfSquares computes the sum of squares for the input for the first N natural numbers.
 func SumOfSquares(n int) int {
 	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference computes the difference of the square of the sum and
-// the sum of squares for the input.
+// the sum of squares for the first N natural numbers.
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
