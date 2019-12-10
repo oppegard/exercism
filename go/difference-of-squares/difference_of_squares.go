00c3fb120063c873e28e4d163ec070b53fc60c76
Glenn Oppegard <goppegard@pivotal.io>
2019-12-10 08:28:05 -0700

    [go] difference-of-squares: solution

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,20 @@
+// Package diffsquares computes the sum of squares.
+// Algorithm credit: https://www.geeksforgeeks.org/difference-between-sum-of-the-squares-of-first-n-natural-numbers-and-square-of-sum/
 package diffsquares
+
+// SquareOfSum computes the square of the sum for the input.
+func SquareOfSum(n int) int {
+	sum := (n * (n + 1)) / 2
+	return sum * sum
+}
+
+// SumOfSquares computes the sum of squares for the input.
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
+}
+
+// Difference computes the difference of the square of the sum and
+// the sum of squares for the input.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
